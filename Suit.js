fefc63f2de6d5fce4cf489b6c7d09c57838009eb
ilfa-sketch <141242297+ilfa-sketch@users.noreply.github.com>
2025-07-28 16:51:34 +0800

    Add files via upload

---
+++ Suit.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Kode JavaScript Anda di sini
+  const player = document.querySelectorAll("ul li img")
+ 
+ function pilComp(){
+     const comp = Math.round(Math.random() *10)
+     if (comp <= 3) return "batu"
+     if (comp >= 3 && comp <= 6) return "gunting"
+     return "kertas"
+ }
+
+
+function HasilAkhir(player,compu){
+    if (player == compu) return "SERI!"
+    if (player == "batu") return (compu == "gunting") ? "MENANG" : "KALAH";
+    if (player == "gunting") return (compu == "kertas") ? "MENANG" : "KALAH";
+    if (player == "kertas") return (compu == "batu") ? "MENANG" : "KALAH";
+    
+}
+
+  for (var i = 0; i < player.length; i++) {
+      player[i].addEventListener("click",function(){
+    const pilcompu = pilComp();
+    const pilplay = this.classList;
+    const hasil = HasilAkhir(pilplay,pilcompu)
+    const Hasil = document.querySelector(".info");
+    Hasil.innerHTML = hasil;
+    const imageComp = document.querySelector(".imgComp")
+    imageComp.setAttribute("src",pilcompu +".png")
+      })
+    
+    
+  }
+  
+});
